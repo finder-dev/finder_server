d096f36c392cc71fa2a5abbb644e2e95025937b3
yoon <dudwls143@gmail.com>
2022-06-18 23:34:04 +0900

    refactor: 태그, 닉네임 사이즈에 대한 조건 변경

--- src/main/java/com/cmc/finder/api/auth/signup/dto/NicknameCheckDto.java
+++ src/main/java/com/cmc/finder/api/auth/signup/dto/NicknameCheckDto.java
@@ -2,19 +2,37 @@ package com.cmc.finder.api.auth.signup.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
 public class NicknameCheckDto {
 
-    private String message;
+    @Getter @Setter
+    public static class Request {
 
-    public static NicknameCheckDto of() {
-
-        return NicknameCheckDto.builder()
-                .message("사용 가능한 닉네임입니다.")
-                .build();
+        @NotBlank
+        @Size(min = 1, max = 6, message = "닉네임은 6자 이내로 적어주세요.")
+        private String nickname;
 
     }
+
+    @Builder
+    @Getter @Setter
+    public static class Response {
+
+        private String message;
+
+        public static Response of() {
+
+            return Response.builder()
+                    .message("사용 가능한 닉네임입니다.")
+                    .build();
+
+        }
+    }
+
 }
--- src/main/java/com/cmc/finder/global/config/WebConfig.java
+++ src/main/java/com/cmc/finder/global/config/WebConfig.java
@@ -1,20 +1,25 @@
 package com.cmc.finder.global.config;
 
-import com.cmc.finder.global.interceptor.AdminAuthorizationInterceptor;
 import com.cmc.finder.global.interceptor.AuthenticationInterceptor;
 import com.cmc.finder.global.resolver.UserEmailArgumentResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @RequiredArgsConstructor
+@EnableWebMvc
 @Configuration
+@ComponentScan("com.cmc.finder")
 public class WebConfig implements WebMvcConfigurer {
 
     private final AuthenticationInterceptor authenticationInterceptor;
@@ -28,7 +33,7 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addInterceptor(authenticationInterceptor)
                 .order(1)
-                .excludePathPatterns("/api/login","/api/signup", "/auth/kakao/callback", "/api/mail/send",
+                .excludePathPatterns("/api/login","/api/signup", "/auth/kakao/callback", "/api/mail/send", "/api/duplicated/**",
                         "/api/health", "/api/oauth/login", "/api/token/reissue", "/api/mail/auth") // 해당 경로는 인터셉터가 가로채지 않는다.
                 .addPathPatterns("/api/**");
 

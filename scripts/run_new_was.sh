f73a7856f697f6c84f398428022942c40ade991d
yoooooooooooon <dudwls143@gmail.com>
2022-06-26 19:50:37 +0900

    Update run_new_was.sh

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -20,6 +20,6 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/finder/build/libs/* > /home/finder/nohup.out 2>&1 &
+nohup java -jar -Djasypt.encryptor.password=dudwls143 -Dserver.port=${TARGET_PORT} /home/finder/build/libs/* > /home/finder/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0

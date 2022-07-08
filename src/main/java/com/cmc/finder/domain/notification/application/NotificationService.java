007e7ff235ef0e730c4ecab1e12c81790b339772
yoon <dudwls143@gmail.com>
2022-07-08 20:45:39 +0900

    feat: 소식 API 구현

--- src/main/java/com/cmc/finder/domain/notification/application/NotificationService.java
+++ src/main/java/com/cmc/finder/domain/notification/application/NotificationService.java
@@ -2,7 +2,11 @@ package com.cmc.finder.domain.notification.application;
 
 import com.cmc.finder.domain.notification.entity.Notification;
 import com.cmc.finder.domain.notification.repository.NotificationRepository;
+import com.cmc.finder.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,9 +18,18 @@ public class NotificationService {
 
     private final NotificationRepository notificationRepository;
 
+    @Transactional
     public void create(Notification notification) {
         notificationRepository.save(notification);
-
     }
+
+    public Page<Notification> getNotificaitons(User user, Pageable pageable) {
+
+        Page<Notification> notifications = notificationRepository.findAllByUser(user, pageable);
+
+        return notifications;
+    }
+
+
 
 }

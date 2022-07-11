4b88ca7b9d74b6775bbf0c0b3ee8545b0bd62411
yoooooooooooon <dudwls143@gmail.com>
2022-07-12 02:00:59 +0900

    Update run_new_was.sh

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -21,6 +21,6 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Dspring.profiles.active=prod  -Djasypt.encryptor.password=${JASYPT_PWD} -Dserver.port=${TARGET_PORT} /home/ec2-user/playground-logging/build/libs/finder-0.0.1-SNAPSHOT.jar > /home/ec2-user/nohup.out 2>&1 &
+nohup java -jar -Dspring.profiles.active=prod -Djasypt.encryptor.password=${JASYPT_PWD} -Dserver.port=${TARGET_PORT} /home/ec2-user/playground-logging/build/libs/finder-0.0.1-SNAPSHOT.jar > /home/ec2-user/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0

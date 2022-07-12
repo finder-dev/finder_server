872b7cdd9b92689c7d242ba09c261c059415df5d
yoon <dudwls143@gmail.com>
2022-07-12 19:34:52 +0900

    feat: 토론 생성 시 validation 추가

--- src/main/java/com/cmc/finder/api/debate/dto/CreateDebateDto.java
+++ src/main/java/com/cmc/finder/api/debate/dto/CreateDebateDto.java
@@ -4,6 +4,7 @@ import com.cmc.finder.domain.debate.entity.Debate;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 
 public class CreateDebateDto {
@@ -11,12 +12,15 @@ public class CreateDebateDto {
     @Getter @Setter
     public static class Request {
 
+        @Size(max = 50, message = "제목은 50자이내로 작성해주세요.")
         @NotBlank(message = "글 제목은 필수값 입니다.")
         private String title;
 
+        @Size(max = 8, message = "선택지는 8자이내로 작성해주세요.")
         @NotBlank(message = "토론 선택지A는 필수값 입니다.")
         private String optionA;
 
+        @Size(max = 8, message = "선택지는 8자이내로 작성해주세요.")
         @NotBlank(message = "토론 선택지B는 필수값 입니다.")
         private String optionB;
 

5cf7cabf1f77c87124a7151a77ad7fc350f5a786
yoon <dudwls143@gmail.com>
2022-07-14 03:28:48 +0900

    feat: 커뮤니티 글 수정 필드 추가

--- src/main/java/com/cmc/finder/domain/community/repository/CommunityRepositoryImpl.java
+++ src/main/java/com/cmc/finder/domain/community/repository/CommunityRepositoryImpl.java
@@ -37,7 +37,7 @@ public class CommunityRepositoryImpl implements CommunityRepositoryCustom {
                 .from(community)
                 .join(community.user, user).fetchJoin()
                 .where(
-                       user.isDeleted.isFalse(), findByMBTI(mbti)
+                       findByMBTI(mbti)
                 )
                 .groupBy(community.communityId)
                 .orderBy(listSort(pageable))
